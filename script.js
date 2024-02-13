c0d24719e61282387d3ef8d8c2dca2d8a7c1301e
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2024-02-13 21:16:32 +0000

    adding highlight logic

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
+  highlightCurrentSong();
   audio.play();
 };
 
@@ -109,7 +110,6 @@ const pauseSong = () => {
 };
 
 const playNextSong = () => {
-
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
@@ -120,8 +120,7 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {
-    const playPreviousSong = () => {
+const playPreviousSong = () => {  
         if (userData?.currentSong === null){
           return
         }else{
@@ -129,7 +128,20 @@ const playPreviousSong = () => {
             const previousSong = userData?.songs[currentSongIndex -1 ]
             playSong(previousSong.id)
         }
-      };
+};
+
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+  
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+
+    if (songToHighlight){
+        songToHighlight.setAttribute("aria-current", "true");
+    }
+
 };
 
 const renderSongs = (array) => {
