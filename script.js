3afe1b322ff5efcaa7aa769bcc9fa84d77975f7c
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2024-02-13 22:28:48 +0000

    adding shuffle feature logic

--- script.js
+++ script.js
@@ -99,6 +99,8 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  playSong(setPlayerDisplay());
+  playSong(setPlayButtonAccessibleText());
   audio.play();
 };
 
@@ -130,11 +132,18 @@ const playPreviousSong = () => {
         }
 };
 
+const shuffle = () => {
+    
+};
+
 const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title");
     const songArtist = document.getElementById("player-song-artist")
     const currentTitle = userData?.currentSong?.title;
     const currentArtist = userData?.currentSong?.artist;
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+    
   };
 
 const highlightCurrentSong = () => {
@@ -171,6 +180,11 @@ const renderSongs = (array) => {
     .join("");
 
   playlistSongs.innerHTML = songsHTML;
+};
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+    playButton.setAttribute("aria-label", song?.title ? `Play${song.title}` : "Play" )
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
